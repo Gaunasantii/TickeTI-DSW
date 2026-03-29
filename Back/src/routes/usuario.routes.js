7c322c34ef5b231fd5d9154a2a3444769a353bba
G-rm4n <oligah06@gmail.com>
2026-03-29 14:10:53 -0300

    ruta login añadida

--- Back/src/routes/usuario.routes.js
+++ Back/src/routes/usuario.routes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const usuarioController = require("../controllers/usuario.controller");
 
 router.post("/usuarios", usuarioController.create);
+router.post("/auth/login",usuarioController.login)
 router.get("/usuarios", usuarioController.getAll);
+
 
 module.exports = router; 
