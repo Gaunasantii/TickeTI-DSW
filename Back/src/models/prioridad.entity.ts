98d4339d04436c6f80b4a9dc59710a57c4e1fb85
Gaston <gastonnicolasroman@gmail.com>
2026-04-22 21:10:14 -0300

    Agrega entidades y tsconfig

---
+++ Back/src/models/prioridad.entity.ts
@@ -0,0 +1,13 @@
+import { defineEntity, p } from '@mikro-orm/core';
+
+export const PrioridadSchema = defineEntity({
+  name: 'prioridad',
+  properties: {
+    id: p.integer().primary().autoincrement(),
+    nombre: p.string(),
+    tiempoLimiteResolucion: p.integer(), // en horas
+  }
+});
+
+export class Prioridad extends PrioridadSchema.class {}
+PrioridadSchema.setClass(Prioridad);
