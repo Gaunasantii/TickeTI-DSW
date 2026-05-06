49c7853d8eb174c44e7a72f630a6d28393a0ddf6
G-rm4n <oligah06@gmail.com>
2026-05-06 00:07:48 -0300

    logs removed

--- Back/src/config/db.ts
+++ Back/src/config/db.ts
@@ -40,9 +40,7 @@ export async function initOrm() {
       host: process.env.DB_HOST as string,
       port: Number(process.env.DB_PORT),
     })
-    console.log('DB conectada ✅')
   } catch (e) {
-    console.error('Error al conectar DB ❌:', e)
     process.exit(1)
   }
 }
@@ -61,31 +59,3 @@ export const  checkDb=async ()=> {
     console.error('DB no conectada ❌', e)
   }
 }
-/*
-// Creamos la instancia de conexión
-const sequelize = new Sequelize(
-    process.env.DB_NAME,     // Nombre de la base de datos
-    process.env.DB_USER,     // Tu usuario (ej: 'root')
-    process.env.DB_PASSWORD, // Tu contraseña
-    {
-        host: process.env.DB_HOST,
-        dialect: 'mysql',
-        logging: false, // Para que no llene la consola de SQL
-    }
-);
-
-// Función para probar la conexión y sincronizar
-const connectDB = async () => {
-    try {
-        await sequelize.authenticate();
-        console.log('✅ Conexión a MySQL exitosa.');
-        
-        // Sincroniza los modelos con la DB (crea las tablas)
-        await sequelize.sync({ alter: true });
-        console.log('✅ Tablas sincronizadas correctamente.');
-    } catch (error) {
-        console.error('❌ Error de conexión a la base de datos:', error.message);
-        process.exit(1); // Detiene la app si no hay base de datos
-    }
-};
-*/
