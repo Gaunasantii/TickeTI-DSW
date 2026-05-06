3e15b6e4c68781fcaa11fdfa3dbcff8c18a29984
G-rm4n <oligah06@gmail.com>
2026-05-06 00:08:09 -0300

    type added

--- Back/src/controllers/estado.controller.ts
+++ Back/src/controllers/estado.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { type Request, type Response } from "express";
 import { orm } from "../config/db.js";
 import { EstadoSchema } from "../models/estado.entity.js";
 
