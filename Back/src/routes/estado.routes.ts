9a771d4dd98d31725b31056cf5dfb31b20e3ca19
G-rm4n <oligah06@gmail.com>
2026-05-06 00:11:11 -0300

    type added

--- Back/src/routes/estado.routes.ts
+++ Back/src/routes/estado.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { estadoController } from "../controllers/estado.controller.js";
 
-export const estadoRouter = Router();
+export const estadoRouter:Router = Router();
 
 estadoRouter.post('/estados',estadoController.createNew);
 estadoRouter.get('/estados',estadoController.findAll)
