69e3aa94f18aaa0188fb8f3fcc0155eec7649e8f
G-rm4n <oligah06@gmail.com>
2026-05-23 12:42:03 -0300

    Implementación del asignacionDAO y refactorización del AsignacionController para utilizar el DAO en las operaciones de creación, búsqueda, actualización y eliminación de asignaciones.

--- Back/src/controllers/asignacion.controller.ts
+++ Back/src/controllers/asignacion.controller.ts
@@ -1,77 +1,61 @@
 import { type Request, type Response } from "express";
-import { orm } from "../config/db.js";
-import { asignacionSchema } from "../models/asignacion.entity.js";
+import { asignacionDAO } from "../DAO/asignacion.DAO.js";
 
-class AsignacionController{
-  async findAll(req:Request,res:Response){
-    try{
-      const em=orm.em.fork();
-      const asignacionesRecovered=await em.findAll(asignacionSchema);
-      res.status(200).json({message:"asignaciones Recuperadas",data:asignacionesRecovered})
-    }catch(error:any){
-      res.status(500).json({error:error.message});
+class AsignacionController {
+  async findAll(req: Request, res: Response) {
+    try {
+      const asignacionesRecovered = await asignacionDAO.findAll({});
+      res.status(200).json({ message: "asignaciones Recuperadas", data: asignacionesRecovered })
+    } catch (error: any) {
+      res.status(500).json({ error: error.message });
     }
   }
 
-  async createAsignacion(req:Request,res:Response){
-        try{
-          const em=orm.em.fork()
-          const asignacionInput=req.body;
-          const newAsignacion=em.create(asignacionSchema, asignacionInput);
-          em.persist(newAsignacion);
-          await em.flush();
-          res.status(201).json({message:"asignacion creada",data:newAsignacion});
-        }catch(error:any){
-          res.status(500).json({error:error.message});
-        }
+  async createAsignacion(req: Request, res: Response) {
+    try {
+      const asignacionInput = req.body;
+      const newAsignacion = await asignacionDAO.createAsignacion(asignacionInput);
+      res.status(201).json({ message: "asignacion creada", data: newAsignacion });
+    } catch (error: any) {
+      res.status(500).json({ error: error.message });
+    }
   }
 
-  async updateAsignacion(req:Request, res:Response){
-      try{
-        const em=orm.em.fork();
-        const {id} = req.params;
-        req.body.fechaCierre=new Date(req.body.fechaCierre)
-        const asignacionInput=req.body
-  
-        const asignacionFound = await em.findOneOrFail(asignacionSchema, {id: Number(id)})
-  
-        em.assign(asignacionFound, asignacionInput);
-  
-        await em.flush();
-  
-        res.status(200).json({
-          message: "asignacion actualizada",
-          data:asignacionFound
-        });
-      } catch (error: any) {
-        res.status(500).json ({
-          error:error.message
-        });
-      }
+  async updateAsignacion(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      req.body.fechaCierre = new Date(req.body.fechaCierre)
+      const asignacionInput = req.body
+
+      const asignacionFound = await asignacionDAO.updateAsignacion(asignacionInput, { id: Number(id) });
+      res.status(200).json({
+        message: "asignacion actualizada",
+        data: asignacionFound
+      });
+    } catch (error: any) {
+      res.status(500).json({
+        error: error.message
+      });
     }
+  }
 
 
-  async deleteAsignacion(req:Request, res:Response){
-      try{
-        const em=orm.em.fork();
-        const {id} = req.params;
-  
-        const asignacionFound = await em.findOneOrFail(asignacionSchema, {id: Number(id)})
-  
-        em.remove(asignacionFound);
-  
-        await em.flush();
-  
-        res.status(200).json({
-          message: "asignacion eliminada",
-          data:asignacionFound
-        });
-      } catch (error: any) {
-        res.status(500).json ({
-          error:error.message
-        });
-      }
+  async deleteAsignacion(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+
+      const asignacionFound = await asignacionDAO.deleteAsignacion({ id: Number(id) });
+
+      res.status(200).json({
+        message: "asignacion eliminada",
+        data: asignacionFound
+      });
+    } catch (error: any) {
+      res.status(500).json({
+        error: error.message
+      });
     }
+  }
 }
 
-export const asignacioncontroller=new AsignacionController();
+export const asignacioncontroller = new AsignacionController();
