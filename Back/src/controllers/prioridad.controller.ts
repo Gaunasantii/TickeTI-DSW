b981a9bfb67c6639e0cf0267372110a6719a5024
G-rm4n <oligah06@gmail.com>
2026-05-23 12:46:11 -0300

    Implementación del prioridadDAO y refactorización del prioridadController para utilizar el DAO en las operaciones de creación, búsqueda, actualización y eliminación de prioridades.

--- Back/src/controllers/prioridad.controller.ts
+++ Back/src/controllers/prioridad.controller.ts
@@ -1,78 +1,60 @@
 import { type Request, type Response } from "express";
-import { orm } from "../config/db.js";
-import { PrioridadSchema } from "../models/prioridad.entity.js";
+import { prioridadDAO } from "../DAO/prioridad.DAO.js";
 
-class prioridadController{
+class prioridadController {
 
-  async createPrioridad(req:Request,res:Response){
-    try{
-      const em=orm.em.fork()
-      const prioridadInput=req.body;
-      const newPrioridad=em.create(PrioridadSchema,prioridadInput);
-      em.persist(newPrioridad);
-      await em.flush();
-      res.status(201).json({message:"Prioridad creado",data:newPrioridad});
-    }catch(error:any){
-      res.status(500).json({error:error.message})
+  async createPrioridad(req: Request, res: Response) {
+    try {
+      const prioridadInput = req.body;
+      const newPrioridad = await prioridadDAO.createPrioridad(prioridadInput);
+      res.status(201).json({ message: "Prioridad creado", data: newPrioridad });
+    } catch (error: any) {
+      res.status(500).json({ error: error.message })
     }
   };
 
-  async findAll(req:Request,res:Response){
-    try{
-      const em=orm.em.fork();
-      const prioridadRecovered=await em.findAll(PrioridadSchema);
-      res.status(200).json({message:"Prioridades Recuperadas",data:prioridadRecovered})
-    }catch(error:any){
-      res.status(500).json({error:error.message});
+  async findAll(req: Request, res: Response) {
+    try {
+      const prioridadRecovered = await prioridadDAO.findAll({});
+      res.status(200).json({ message: "Prioridades Recuperadas", data: prioridadRecovered })
+    } catch (error: any) {
+      res.status(500).json({ error: error.message });
     }
   }
 
-  async updatePrioridad(req:Request, res:Response){
-    try{
-      const em=orm.em.fork();
-      const {id} = req.params;
+  async updatePrioridad(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
       const prioridadinput = req.body;
 
-      const prioridadfound = await em.findOneOrFail(PrioridadSchema, {id: Number(id)})
-
-      em.assign(prioridadfound , prioridadinput);
-
-      await em.flush();
-
+      const prioridadUpdated = await prioridadDAO.updatePrioridad(prioridadinput, { id: Number(id) });
       res.status(200).json({
         message: "Prioridad actualizada",
-        data:prioridadfound
+        data: prioridadUpdated
       });
     } catch (error: any) {
-      res.status(500).json ({
-        error:error.message
+      res.status(500).json({
+        error: error.message
       });
     }
   }
 
-  async deletePrioridad(req:Request, res:Response){
-    try{
-      const em=orm.em.fork();
-      const {id} = req.params;
+  async deletePrioridad(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
 
-      const prioridadfound = await em.findOneOrFail(PrioridadSchema, {id: Number(id)})
-
-      em.remove(prioridadfound);
-
-      await em.flush();
+      const prioridadToDelete = await prioridadDAO.deletePrioridad({ id: Number(id) });
 
       res.status(200).json({
-        message: "Prioridad eliminado",
-        data:prioridadfound
+        message: "Prioridad eliminada",
+        data: prioridadToDelete
       });
     } catch (error: any) {
-      res.status(500).json ({
-        error:error.message
+      res.status(500).json({
+        error: error.message
       });
     }
   }
-
-
 }
 
 export const prioridadcontroller = new prioridadController();
