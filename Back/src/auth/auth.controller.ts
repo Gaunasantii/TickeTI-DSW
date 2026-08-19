efc203e657dbc3fa1af994da7dcf657370dd0a50
G-rm4n <oligah06@gmail.com>
2026-08-19 18:15:18 -0300

    Cambio en manejo de error en auth

--- Back/src/auth/auth.controller.ts
+++ Back/src/auth/auth.controller.ts
@@ -5,11 +5,12 @@ export class AuthController {
 
   async login(req: Request, res: Response) {
     try {
-      const { mail, pass } = req.body;
-      const token=AuthService.Login(pass,mail);
-      res.cookie('AccesToken', token,{httpOnly:true});
+      const { email, pass } = req.body;
+      const token = await AuthService.Login(pass, email);
+      res.cookie('AccessToken', token, { httpOnly: true });
+      res.status(200).json({ message: "Inicio de sesion exitoso" });
     } catch (error: any) {
-      res.status(500).json({ error: error.message });
+      res.status(401).json({ error: error.message });
     };
   }
 }
--- Back/src/auth/auth.services.ts
+++ Back/src/auth/auth.services.ts
@@ -1,12 +1,14 @@
 import { authDAO } from "./auth.DAO.js";
 import { JWToken } from "../utils/jwt.js";
 import { Person } from "../persona/person.entity.js";
-import { Jwt } from "jsonwebtoken";
 
-export class AuthService{
-    static async Login(pass:string,email:string){
-      const user:Person = await authDAO.getUser(email,pass);
-      const token=JWToken.createToken(user)
-      return token;
+export class AuthService {
+  static async Login(pass: string, email: string) {
+    const user: Person | null = await authDAO.getUser(email, pass);
+    if (!user) throw new Error("Credenciales invalidas");
+    //aqui habria que colocar el metodo con el hash
+    if (user.pass != pass) throw new Error("Credenciales invalidas");
+    const token = JWToken.createToken(user)
+    return token;
   }
 }
