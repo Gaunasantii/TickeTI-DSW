ef680a9b4e93fb89e75e6ee0703166d67fb02814
G-rm4n <oligah06@gmail.com>
2026-09-19 00:23:03 -0300

    refact

--- Back/src/tecnico/tecnico.controller.ts
+++ Back/src/tecnico/tecnico.controller.ts
@@ -2,60 +2,35 @@ import { type Request, type Response } from "express";
 import { tecnicoDAO } from "./tecnico.DAO.js";
 import { TecnicoDTO } from "./DTO/tecnico.dto.js";
 import { TecnicoService } from "./tecnico.service.js";
+import { ApiSuccessResponse } from "../utils/api.response.js";
 
 class tecnicoController {
 
   async createTecnico(req: Request, res: Response) {
-    try {
       const tecnicoInput = req.body;
-      const newTecnico = await TecnicoService.createTecnico(tecnicoInput);
+      await TecnicoService.createTecnico(tecnicoInput);
 
-      res.status(200).json({ message: "Tecnico creado", data: newTecnico });
-    } catch (error: any) {
-      res.status(500).json({ error: error.message })
-    }
+      res.status(201).json(new ApiSuccessResponse<null>(null,"Tecnico creado con exito"));
   };
 
   async findAll(req: Request, res: Response) {
-    try {
       const tecnicosRecovered = await TecnicoService.getAllTecnicos()
-
-      res.status(200).json({ message: "Tecnicos Recuperados", data: tecnicosRecovered })
-    } catch (error: any) {
-      res.status(500).json({ error: error.message });
-    }
+      res.status(200).json(new ApiSuccessResponse<TecnicoDTO[]>(tecnicosRecovered,"Tecnicos recuperados con exito"))
   }
 
   async updateTecnico(req: Request, res: Response) {
-    try {
+    
       const dni = req.params.dni as string;
       const tecnicoinput = req.body;
       const updatedTecnico = await TecnicoService.updateTecnico(dni, tecnicoinput)
 
-      return res.status(200).json({
-        message: "Tecnico actualizado",
-        data: updatedTecnico
-      });
-    } catch (error: any) {
-      return res.status(404).json({
-        error: error.message
-      });
-    }
+      return res.status(200).json(new ApiSuccessResponse<null>(null,"Tecnico actualizado con exitos"));
   }
 
   async deleteTecnico(req: Request, res: Response) {
-    try {
       const dni = req.params.dni as string;
       await TecnicoService.deleteTecnico(dni)
-
-      return res.status(204).json({
-        message: "Tecnico eliminado",
-      });
-    } catch (error: any) {
-      return res.status(404).json({
-        error: error.message
-      });
-    }
+      return res.status(200).json(new ApiSuccessResponse<null>(null,"Tecnico eliminado con exito"));
   }
 }
 export const tecnicocontroller = new tecnicoController();
