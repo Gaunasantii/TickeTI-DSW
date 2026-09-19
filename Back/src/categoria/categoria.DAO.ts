2d1d9d5d3fec5a4473f059c0323b0434be7d1a1f
G-rm4n <oligah06@gmail.com>
2026-09-18 22:11:00 -0300

    refac:Refactoriza dao, controller y service de categoria para adaptarlos al nuevo errorHandler

--- Back/src/categoria/categoria.DAO.ts
+++ Back/src/categoria/categoria.DAO.ts
@@ -1,59 +1,38 @@
 import { orm } from "../config/db.js";
+import { mapDbErrorToAppError } from "../utils/DbErrorMapper.js";
 import { CategoriaSchema } from "./categoria.entity.js";
 
 export class categoriaDAO {
   static async findAll(filters: any) {
-    try {
       const em = orm.em.fork();
       const categoriaRecovered = await em.findAll(CategoriaSchema, filters);
       return categoriaRecovered;
-    } catch (error: any) {
-      throw new Error(error.message);
-    }
   }
 
   static async findOne(filters: any) {
-    try {
       const em = orm.em.fork();
-      const categoriaFound = await em.findOneOrFail(CategoriaSchema, filters);
+      const categoriaFound = await em.findOne(CategoriaSchema, filters);
       return categoriaFound;
-    } catch (error: any) {
-      throw new Error(error.message);
-    }
   }
 
   static async createCategoria(categoriaInput: any) {
-    try {
       const em = orm.em.fork();
       const newCategoria = em.create(CategoriaSchema, categoriaInput);
       em.persist(newCategoria);
-      await em.flush();
+      await em.flush().catch((error:any)=>mapDbErrorToAppError(error));
       return newCategoria;
-    } catch (error: any) {
-      throw new Error(error.message);
-    }
-  }
-  static async updateCategoria(categoriaInput: any, filters: any) {
-    try {
-      const em = orm.em.fork();
-      const categoriaToUpdate = await em.findOneOrFail(CategoriaSchema, filters);
-      em.assign(categoriaToUpdate, categoriaInput);
-      await em.flush();
-      return categoriaToUpdate;
-    } catch (error: any) {
-      throw new Error(error.message);
-    }
   }
 
-  static async deleteCategoria(filters: any) {
-    try {
+  static async updateCategoria(categoriaInput: any, categoriaFound:any) {
       const em = orm.em.fork();
-      const categoriaToDelete = await em.findOneOrFail(CategoriaSchema, filters);
-      em.remove(categoriaToDelete);
+      em.assign(categoriaFound, categoriaInput);
+      await em.flush().catch((error:any)=>mapDbErrorToAppError(error));
+      return categoriaFound
+  }
+
+  static async deleteCategoria(categoriaFound:any) {
+      const em = orm.em.fork();
+      em.remove(categoriaFound)
       await em.flush();
-      return categoriaToDelete;
-    } catch (error: any) {
-      throw new Error(error.message);
-    }
   }
 }
