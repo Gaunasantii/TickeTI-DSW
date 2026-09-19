158ea513d5bd9d18ba20541d922430fbaac41a69
G-rm4n <oligah06@gmail.com>
2026-09-18 22:40:26 -0300

    refact:Refactoriza service, controller y dao de empresa para adaptarlos a el nuevo errorHandler

--- Back/src/empresa/empresa.DAO.ts
+++ Back/src/empresa/empresa.DAO.ts
@@ -1,59 +1,38 @@
 import { orm } from "../config/db.js";
+import { mapDbErrorToAppError } from "../utils/DbErrorMapper.js";
 import { EmpresaSchema } from "./empresa.entity.js";
 
 export class empresaDAO {
   static async findAll(filters: any) {
-    try {
       const em = orm.em.fork();
       const empresaRecovered = await em.findAll(EmpresaSchema, filters);
       return empresaRecovered;
-    } catch (error: any) {
-      throw new Error(error.message);
-    }
   }
 
   static async findOne(filters: any) {
-    try {
       const em = orm.em.fork();
-      const empresaFound = await em.findOneOrFail(EmpresaSchema, filters);
+      const empresaFound = await em.findOne(EmpresaSchema, filters);
       return empresaFound;
-    } catch (error: any) {
-      throw new Error(error.message);
-    }
   }
 
   static async createEmpresa(empresaInput: any) {
-    try {
       const em = orm.em.fork();
       const newEmpresa = em.create(EmpresaSchema, empresaInput);
       em.persist(newEmpresa);
-      await em.flush();
+      await em.flush().catch((error:any)=>mapDbErrorToAppError(error));
       return newEmpresa;
-    } catch (error: any) {
-      throw new Error(error.message);
-    }
   }
-  static async updateEmpresa(empresaInput: any, filters: any) {
-    try {
+  static async updateEmpresa(empresaInput: any, empresaFound:any) {
       const em = orm.em.fork();
-      const empresaToUpdate = await em.findOneOrFail(EmpresaSchema, filters);
-      em.assign(empresaToUpdate, empresaInput);
-      await em.flush();
-      return empresaToUpdate;
-    } catch (error: any) {
-      throw new Error(error.message);
-    }
+      em.assign(empresaFound, empresaInput);
+      await em.flush().catch((error:any)=>mapDbErrorToAppError(error));
+      return empresaFound;
   }
 
-  static async deleteEmpresa(filters: any) {
-    try {
+  static async deleteEmpresa(empresaFound:any) {
       const em = orm.em.fork();
-      const empresaToDelete = await em.findOneOrFail(EmpresaSchema, filters);
-      em.remove(empresaToDelete);
+      em.remove(empresaFound);
       await em.flush();
-      return empresaToDelete;
-    } catch (error: any) {
-      throw new Error(error.message);
-    }
+      return empresaFound;
   }
 }
--- Back/src/empresa/empresa.controller.ts
+++ Back/src/empresa/empresa.controller.ts
@@ -2,57 +2,32 @@ import { type Request, type Response } from "express";
 import { empresaDAO } from "./empresa.DAO.js";
 import { EmpresaDTO } from "./DTO/empresa.dto.js";
 import { EmpresaService } from "./empresa.service.js";
+import { ApiSuccessResponse } from "../utils/api.response.js";
 
 class empresaController {
 
   async createEmpresa(req: Request, res: Response) {
-    try {
       const empresaInput = req.body;
-      const newEmpresa = EmpresaService.createEmpresa(empresaInput)
-
-      res.status(200).json({ message: "Empresa creada", data: newEmpresa });
-    } catch (error: any) {
-      res.status(500).json({ error: error.message })
-    }
+      EmpresaService.createEmpresa(empresaInput)
+      res.status(201).json(new ApiSuccessResponse<null>(null,"Empresa creada con exito"));
   };
 
   async findAll(req: Request, res: Response) {
-    try {
       const empresas = await EmpresaService.getAllEmpresas()
-      res.status(200).json({ message: "Empresa Recuperadas", data: empresas })
-    } catch (error: any) {
-      res.status(500).json({ error: error.message });
-    }
+      res.status(200).json(new ApiSuccessResponse<EmpresaDTO[]>(empresas,"Empresas recuperadas con exito"))
   }
 
   async updateEmpresa(req: Request, res: Response) {
-    try {
       const id = Number(req.params.id);
       const empresainput = req.body;
       const updatedEmpresa = await EmpresaService.updateEmpresa(id, empresainput)
-      res.status(200).json({
-        message: "Empresa actualizada",
-        data: updatedEmpresa
-      });
-    } catch (error: any) {
-      res.status(404).json({
-        error: error.message
-      });
-    }
+      res.status(200).json(new ApiSuccessResponse<EmpresaDTO>(updatedEmpresa,"Empresa actualizada con exito"));
   }
 
   async deleteEmpresa(req: Request, res: Response) {
-    try {
       const id = Number(req.params.id);
       await EmpresaService.deleteEmpresa(id)
-      res.status(204).json({
-        message: "Empresa eliminado",
-      });
-    } catch (error: any) {
-      res.status(404).json({
-        error: error.message
-      });
-    }
+      res.status(200).json(new ApiSuccessResponse<null>(null,"Empresa eliminada con exito"));
   }
 }
 
